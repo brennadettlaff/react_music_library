1a73a23b13cf32d32c293bb9e884d059e51c3a02
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-20 19:49:55 -0600

    added display music component and instantiated component on app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, { useState } from 'react';
+import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
 
 function App() {
+
+  const [entries, setMusic] = useState([{title: 'title', artist: 'artist', album:'album', release_date:'date', genre: 'genre'}, {title: 'title2', artist: 'artist2', album:'album2', release_date:'date2', genre: 'genre2'}])
+
   return (
     <div>
-      
+      <DisplayMusic parentEntries={entries} />
     </div>
   );
 }
