fc258072649c069ad42dd25530dd9187b21802ea
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-21 09:50:17 -0600

    installed axios + created functionality to pull data from music library api project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
+import axios from 'axios';
 
 function App() {
 
-  const [entries, setMusic] = useState([{title: 'title', artist: 'artist', album:'album', release_date:'date', genre: 'genre'}, {title: 'title2', artist: 'artist2', album:'album2', release_date:'date2', genre: 'genre2'}])
+
+  const [entries, setMusic] = useState([])
+
+  useEffect(() => {
+    getAllMusic();
+  }, [])
+
+  async function getAllMusic(){
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response.data)
+    setMusic(response.data);
+  }
 
   return (
     <div>
