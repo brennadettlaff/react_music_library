067b3004a5006081e53d059c81be9edf639d75ce
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-19 18:40:59 -0600

    added style page for add music component

--- src/Components/AddMusic/AddMusic.jsx
+++ src/Components/AddMusic/AddMusic.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './AddMusic.css'
 
 const AddMusic = (props) => {
     const [title, setTitle] = useState('');
@@ -21,23 +22,23 @@ const AddMusic = (props) => {
 
     return ( 
         <form onSubmit={handleSubmit}>
-            <div>
+            <div className='formtable'>
                 <label>Title:</label>
                 <input type='text' value={title} onChange={(event) => setTitle(event.target.value)}></input>
             </div>
-            <div>
+            <div className='formtable'>
                 <label>Artist:</label>
                 <input type='text' value={artist} onChange={(event) => setArtist(event.target.value)}></input>
             </div>
-            <div>
+            <div className='formtable'>
                 <label>Album:</label>
                 <input type='text' value={album} onChange={(event) => setAlbum(event.target.value)}></input>
             </div>
-            <div>
+            <div className='formtable'>
                 <label>Release Date:</label>
                 <input type='date' value={release_date} onChange={(event) => setReleaseDate(event.target.value)}></input>
             </div>
-            <div>
+            <div className='formtable'>
                 <label>Genre:</label>
                 <input type='text' value={genre} onChange={(event) => setGenre(event.target.value)}></input>
             </div>
